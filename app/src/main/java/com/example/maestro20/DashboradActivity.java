06348d1f6b512906160260465645834cadb1ce9a
StarDust199 <agnieszka.hamal@gmail.com>
2023-07-25 12:25:49 +0200

    register + login vol.1

---
+++ app/src/main/java/com/example/maestro20/DashboradActivity.java
@@ -0,0 +1,17 @@
+package com.example.maestro20;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DashboradActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+    }
+}
--- app/src/main/java/com/example/maestro20/RegisterActivity.java
+++ app/src/main/java/com/example/maestro20/RegisterActivity.java
@@ -1,14 +1,73 @@
 package com.example.maestro20;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class RegisterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    EditText editLogin, editPassword, editEmail;
+    Button regButton;
+    Spinner roleSpinner;
+    String item;
+    FirebaseDatabase database;
+    DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        editLogin=findViewById(R.id.login);
+        editPassword=findViewById(R.id.password);
+        editEmail=findViewById(R.id.email);
+        regButton=findViewById(R.id.button_register);
+        roleSpinner = findViewById(R.id.role_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.user_roles, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        roleSpinner.setAdapter(adapter);
+
+        TextView txtView = findViewById(R.id.textView);
+        txtView.setOnClickListener(view -> {
+            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        regButton.setOnClickListener(view -> {
+            database=FirebaseDatabase.getInstance();
+            reference= database.getReference("users");
+            String login=editLogin.getText().toString();
+            String email=editEmail.getText().toString();
+            String password=editPassword.getText().toString();
+
+
+            HelperClass helperClass=new HelperClass(login,password,email,item);
+            reference.child(login).setValue(helperClass);
+            Toast.makeText(RegisterActivity.this, "Konto zostało utworzone!",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+            startActivity(intent);
+
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        item=roleSpinner.getSelectedItem().toString();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
