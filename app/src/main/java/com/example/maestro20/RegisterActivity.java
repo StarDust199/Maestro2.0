76c0e170416e7311902fc313febc1963a4069efd
StarDust199 <agnieszka.hamal@gmail.com>
2023-07-28 09:32:41 +0200

    working create user acc

--- app/src/main/java/com/example/maestro20/RegisterActivity.java
+++ app/src/main/java/com/example/maestro20/RegisterActivity.java
@@ -1,7 +1,13 @@
 package com.example.maestro20;
 
+import static android.content.ContentValues.TAG;
+
+import static com.example.maestro20.HelperClass.role;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,8 +17,13 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -22,8 +33,12 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
     Button regButton;
     Spinner roleSpinner;
     String item;
-    FirebaseDatabase database;
-    DatabaseReference reference;
+    FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,30 +48,45 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
         editEmail=findViewById(R.id.email);
         regButton=findViewById(R.id.button_register);
         roleSpinner = findViewById(R.id.role_spinner);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.user_roles, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         roleSpinner.setAdapter(adapter);
-
+        mAuth = FirebaseAuth.getInstance();
         TextView txtView = findViewById(R.id.textView);
         txtView.setOnClickListener(view -> {
             Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
             startActivity(intent);
         });
 
+
+
         regButton.setOnClickListener(view -> {
-            database=FirebaseDatabase.getInstance();
-            reference= database.getReference("users");
-            String login=editLogin.getText().toString();
-            String email=editEmail.getText().toString();
-            String password=editPassword.getText().toString();
 
 
-            HelperClass helperClass=new HelperClass(login,password,email,item);
-            reference.child(login).setValue(helperClass);
-            Toast.makeText(RegisterActivity.this, "Konto zostało utworzone!",Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-            startActivity(intent);
+            String login = editLogin.getText().toString();
+            String email = editEmail.getText().toString();
+            String password = editPassword.getText().toString();
 
+            if (TextUtils.isEmpty(login) || TextUtils.isEmpty(password) || TextUtils.isEmpty(email)) {
+
+                Toast.makeText(RegisterActivity.this, "Pole nie może być puste", Toast.LENGTH_SHORT).show();
+            }
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(RegisterActivity.this, "Konto zostało utworzone.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Tworzenie konta nie powiodło się.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+            writeNewUser(login, password, email, role);
         });
     }
 
@@ -70,4 +100,11 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
     public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
+
+    public void writeNewUser(String login, String password, String email, String role) {
+        HelperClass user = new HelperClass(login,password, email,role);
+
+        mDatabase.child("users").child(login).setValue(user);
+    }
+
 }
